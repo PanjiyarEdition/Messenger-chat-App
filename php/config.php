c1830054132d4009107861863d1c381d89f19e84
JIGAR PANJIYAR <133746395+PanjiyarEdition@users.noreply.github.com>
2023-11-02 01:23:47 +0530

    Update config.php

--- php/config.php
+++ php/config.php
@@ -2,7 +2,7 @@
   $hostname = "localhost";
   $username = "root";
   $password = "";
-  $dbname = "panjiyar_edition messenger";
+  $dbname = "panjiyar_edition_messenger";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
   if(!$conn){
